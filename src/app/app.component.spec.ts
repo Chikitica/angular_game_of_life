d3889dc70369b53beddfa0287b96a52f1c97648a
Chikitica <monikmayo91@gmail.com>
2022-07-01 22:27:11 +0200

    Test NumToArray pipe.

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,11 +1,13 @@
 import { TestBed } from '@angular/core/testing';
 import { AppComponent } from './app.component';
+import { NumToArrayPipe } from './pipes/num-to-array.pipe';
 
 describe('AppComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       declarations: [
-        AppComponent
+        AppComponent,
+        NumToArrayPipe
       ],
     }).compileComponents();
   });
@@ -14,18 +16,5 @@ describe('AppComponent', () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
     expect(app).toBeTruthy();
-  });
-
-  it(`should have as title 'game_of_life'`, () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app.title).toEqual('game_of_life');
-  });
-
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('game_of_life app is running!');
   });
 });
--- src/app/pipes/num-to-array.pipe.spec.ts
+++ src/app/pipes/num-to-array.pipe.spec.ts
@@ -1,8 +1,12 @@
 import { NumToArrayPipe } from './num-to-array.pipe';
 
 describe('NumToArrayPipe', () => {
+  const pipe = new NumToArrayPipe();
   it('create an instance', () => {
-    const pipe = new NumToArrayPipe();
     expect(pipe).toBeTruthy();
+  });
+
+  it('transform number 2 to array of [0,1]', () => {
+    expect(pipe.transform(2)).toEqual([0,1]);
   });
 });
